d690963c7973341fc61d99ee74150a75c478c136
Rebekah Amerson <becka_811@hotmail.com>
2018-05-26 15:51:13 -0500

    Add function to set gameboard

--- js/app.js
+++ js/app.js
@@ -36,3 +36,12 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+let moveCounter = 0; //move counter
+let gameBoard = [];
+let flippedCards = [];
+let matchedCards = []
+
+function setGameBoard(){
+  gameBoard.push(document.getElementsByClassName('card'));
+  console.log (gameBoard.toString());
+}
